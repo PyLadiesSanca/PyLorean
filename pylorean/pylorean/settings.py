1531094681290e2b6207ca0fd4199b008037673d
Luiz Menezes <luiz.menezesf@gmail.com>
2015-12-05 19:03:06 -0200

    Fixes base template

--- pylorean/pylorean/settings.py
+++ pylorean/pylorean/settings.py
@@ -104,7 +104,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-print(STATICFILES_DIR)
