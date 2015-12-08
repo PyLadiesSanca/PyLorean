8a6e8f06054bd44dd037e5ddd93b807c34e3030e
julianaklulo <julianaklulo@gmail.com>
2015-12-08 17:56:10 -0200

    Added login feature, but it's not working quite right yet

--- pylorean/core/views.py
+++ pylorean/core/views.py
@@ -1,5 +1,26 @@
-from django.views.generic import TemplateView
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect, HttpRequest, HttpResponsePermanentRedirect, HttpResponseGone
+from django.views.generic import RedirectView, TemplateView
+from django.views.generic.edit import FormView
 
 
 class IndexView(TemplateView):
-    template_name = 'core/index.html'
+    template_name = 'core/index.html' 
+
+
+class LoginView(FormView):
+    template_name = 'core/login.html'
+    form_class = AuthenticationForm
+    success_url = '/'
+
+class LogoutView(RedirectView):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        url = self.get_redirect_url(*args, **kwargs)
+        if url:
+            if self.permanent:
+                return HttpResponsePermanentRedirect(url)
+            else:
+                return HttpResponseRedirect(url)
+        return HttpResponseGone()
--- pylorean/pylorean/urls.py
+++ pylorean/pylorean/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from core.views import IndexView
+from core.views import IndexView, LoginView, LogoutView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^login/$', LoginView.as_view(), name='login'),
+    url(r'^logout/$', LogoutView.as_view(), name='logout'),
 ]
