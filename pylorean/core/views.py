102759f23117b7f8722318eb407158914c5960d5
julianaklulo <julianaklulo@gmail.com>
2015-12-09 15:07:56 -0200

    Login and logout working flawlessly

--- pylorean/core/views.py
+++ pylorean/core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.http import HttpResponseRedirect, HttpRequest, HttpResponsePermanentRedirect, HttpResponseGone
 from django.views.generic import RedirectView, TemplateView
@@ -13,8 +13,19 @@ class LoginView(FormView):
     template_name = 'core/login.html'
     form_class = AuthenticationForm
     success_url = '/'
+    def my_view(request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                # Redirect to a success page.
+                sucess_url = '/' 
+        
 
 class LogoutView(RedirectView):
+    url = '/login'
     def get(self, request, *args, **kwargs):
         logout(request)
         url = self.get_redirect_url(*args, **kwargs)
